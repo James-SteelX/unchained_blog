57ee1031916d0d3da55b6b7f569d729f13de847d
James <James.Steel@me.com>
2016-11-22 12:19:43 +0000

    user authentication complete, user can log in/out/edit own posts only

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.utils import timezone
+
 from .models import Post
+
 from .forms import PostForm
+from .forms import RegistrationForm
+
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render_to_response
+
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.template.context_processors import csrf
 
 # Create your views here.
 def post_list(request):
@@ -29,14 +38,35 @@ def post_new(request):
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+    if request.user.id == post.author_id:
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
     else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+        return render(request, 'blog/incorrect_permissions.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/accounts/register/complete')
+
+    else:
+        form = RegistrationForm()
+    token = {}
+    token.update(csrf(request))
+    token['form'] = form
+
+    return render_to_response('registration/registration_form.html', token)
+
+def registration_complete(request):
+    return render_to_response('registration/registration_complete.html')
